63026dd75d0bf19ba243ed9255e4a84ac7681907
“Garrett <44751888+garrettstevenkeyes@users.noreply.github.com>
2020-09-12 21:08:52 -0400

    adding file classes, issue with blob object

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -13,46 +13,48 @@ import org.springframework.ui.Model;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/home")
+@Controller
 public class HomeController {
-
-    private NoteService noteService;
-    private FileService fileService;
+    @RequestMapping("/home")
+    public String getHomePage(Model model){
+        return "home";
+    }
+//    private NoteService noteService;
+//    private FileService fileService;
 
     //I'll be adding the other services to this
-    public HomeController(NoteService noteService, FileService fileService) {
-        this.noteService = noteService;
-        this.fileService = fileService;
-    }
+//    public HomeController(NoteService noteService, FileService fileService) {
+//        this.noteService = noteService;
+//        this.fileService = fileService;
+//    }
 
     // take us back to the home page when notes are submitted
-    @GetMapping
-    public String getNotePage(NoteForm noteForm, Model model) {
-        model.addAttribute("pageNotes", this.noteService.getPageNotes());
-        return "home";
-    }
+//    @GetMapping
+//    public String getNotePage(NoteForm noteForm, Model model) {
+//        model.addAttribute("pageNotes", this.noteService.getPageNotes());
+//        return "home";
+//    }
 
-    @GetMapping
-    public String getFilePage(FileForm fileForm, Model model) {
-        model.addAttribute("pageFiles", this.fileService.getPageFiles());
-        return "home";
-    }
+//    @GetMapping("/files")
+//    public String getFilePage(FileForm fileForm, Model model) {
+//        model.addAttribute("pageFiles", this.fileService.getPageFiles());
+//        return "home";
+//    }
 
     // After the page has initially been loaded it should do be able to add notes to the page
-    @PostMapping
-    public String postNewNote(Authentication authentication, NoteForm noteForm, Model model) {
-        this.noteService.addNote(noteForm);
-        model.addAttribute("pageNotes", this.noteService.getPageNotes());
-        return "home";
-    }
+//    @PostMapping
+//    public String postNewNote(Authentication authentication, NoteForm noteForm, Model model) {
+//        this.noteService.addNote(noteForm);
+//        model.addAttribute("pageNotes", this.noteService.getPageNotes());
+//        return "home";
+//    }
 
     // i need to add in html mapped tags
 
-//    @PostMapping
-    public String postNewFile(Authentication authentication, FileForm fileForm, Model model){
-        this.fileService.addFile(fileForm);
-        model.addAttribute("pageFiles", this.fileService.getPageFiles());
-        return "home";
-    }
+////    @PostMapping
+//    public String postNewFile(Authentication authentication, FileForm fileForm, Model model){
+//        this.fileService.addFile(fileForm);
+//        model.addAttribute("pageFiles", this.fileService.getPageFiles());
+//        return "home";
+//    }
 }
