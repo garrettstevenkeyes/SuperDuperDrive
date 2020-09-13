34044a34360270024b3a5e8da59cf83cb5074d37
“Garrett <44751888+garrettstevenkeyes@users.noreply.github.com>
2020-09-13 00:26:43 -0400

    adding file classes, issue with blob object

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,4 +1,5 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
+import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
@@ -7,7 +8,7 @@ import org.springframework.ui.Model;
 public class HomeController {
     @RequestMapping("/home")
     public String getHomePage(Model model){
+        model.addAttribute("noteForm", new NoteForm());
         return "home";
     }
 }
-//tea
