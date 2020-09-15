5577a45452c54a741a20a55b67afaef1333ccfe3
“Garrett <44751888+garrettstevenkeyes@users.noreply.github.com>
2020-09-14 21:36:57 -0400

    updating file service

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -1,14 +1,23 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import com.udacity.jwdnd.course1.cloudstorage.model.FileForm;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 
 @Controller
 public class FileController {
     private FileService fileService;
+    private MultipartFile fileUpload;
+    private Model model;
+    private UserService userService;
 
     public FileController(FileService fileService){
         this.fileService = fileService;
@@ -21,9 +30,9 @@ public class FileController {
     }
 
     @PostMapping("/files")
-    public String postNewFile(FileForm fileForm, Model model){
-        this.fileService.addFile(fileForm);
-        model.addAttribute("pageFiles", this.fileService.getPageFiles());
+    public String postNewFile(@RequestParam("fileUpload") MultipartFile fileUpload, Model model, Authentication authentication){
+        int userId = userService.getUser(authentication.name()).getUserId();
+        this.fileService.addFile(new File(null, fileUpload.getOriginalFilename(), fileUpload.getContentType(), String.valueOf(fileUpload.getSize()), userId, fileUpload.getBytes()));
         return "home";
     }
 
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -1,13 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
-
 import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import java.security.SecureRandom;
 import java.util.Base64;
-
 
 @Service
 public class UserService {
